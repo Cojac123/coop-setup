6674850a44c832290f04bf5bdd84f256c02499c9
Cojac123 <cojac808@gmail.com>
2025-10-21 02:07:05 -0500

    last touch up

--- Assets/PlayerLogic.cs
+++ Assets/PlayerLogic.cs
@@ -6,13 +6,19 @@ public class PlayerLogic : MonoBehaviour
     [SerializeField] float moveSpeed = 1;
     [SerializeField] float jumpForce = 10;
     [SerializeField] int playerIndex = 1;
+
+    [SerializeField]
+    Material[] materials;
     
     Vector2 input;
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+        playerIndex = FindObjectsByType(typeof(PlayerLogic), FindObjectsSortMode.InstanceID).Length;
+
+        GetComponent<MeshRenderer>().material = materials[playerIndex - 1];
     }
 
     // Update is called once per frame
